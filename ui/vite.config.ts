3429d594c579761187d398d8d22fc342b63139ee
andyrift <ahramigamedev@yandex.com>
2024-04-30 01:44:00 +0300

    bugfix

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    port: 4000,
+    strictPort: true
+  },
+  preview: {
+    port: 3000,
+    strictPort: true
   }
 })
